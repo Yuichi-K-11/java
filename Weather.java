79c522e3affe2022bb7426ecaceb80f3a9404dc8
Yuichi-K-11 <banaichi1@gmail.com>
2021-04-26 23:40:15 +0900

    [add]weather

---
+++ Weather.java
@@ -0,0 +1,11 @@
+public class Weather{
+  public static void main(String[] args){
+    boolean tenki = false;
+    if (tenki == true){
+      System.out.println("洗濯をする");
+      System.out.println("散歩へGo");
+    } else {
+      System.out.println("Netflixを見る");
+    }
+  }
+ }
