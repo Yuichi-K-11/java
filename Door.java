b697533b5345c4d678507e7c7b5f6e93e5e61329
Yuichi-K-11 <banaichi1@gmail.com>
2021-04-27 21:29:48 +0900

    [add]door

---
+++ Door.java
@@ -0,0 +1,10 @@
+public class Door {
+  public static void main(String[] args) {
+    boolean doorClose = true;     
+    while(doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
+    }
+      System.out.println("トイレに入る");
+  }
+}
