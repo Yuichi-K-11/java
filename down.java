671c44c00945d8dffa3728a04a1e3c9bace0da10
Yuichi-K-11 <banaichi1@gmail.com>
2021-04-30 23:45:35 +0900

    [add]down

---
+++ down.java
@@ -0,0 +1,14 @@
+class down{
+  public static void main(String[] args){
+    int num = 6;
+
+    do{
+      System.out.println("num = " + num);
+      num -= 2;
+
+      if (num < 0){
+        break;
+      }
+    }while (true);
+  }
+}
