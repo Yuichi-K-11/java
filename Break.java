c0beccf5c1ace29484ea3019b866ea18bce5108e
Yuichi-K-11 <banaichi1@gmail.com>
2021-05-02 23:58:55 +0900

    [add]break

---
+++ Break.java
@@ -0,0 +1,13 @@
+class Break{
+  public static void main(String args[]) {
+
+    String str = "処理します";
+    int i=0;
+
+    while(i < 10) {
+       System.out.println( str + i );
+       i++;
+       break;
+    }
+  }
+}
