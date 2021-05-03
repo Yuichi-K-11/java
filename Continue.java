c684dbc3757c55bec053dc6188685372feb35c4a
Yuichi-K-11 <banaichi1@gmail.com>
2021-05-03 21:30:16 +0900

    [add]continue

---
+++ Continue.java
@@ -0,0 +1,13 @@
+class Continue {
+   public static void main(String args[]) {
+
+      String str = "処理します";
+      int i=1;
+
+      while(i < 10) {
+         System.out.println( str + i );
+         i++;
+         break;
+      }
+   }
+}
