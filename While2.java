0118018436017f720e0dd316a89a16a120a3933b
Yuichi-K-11 <banaichi1@gmail.com>
2021-05-06 20:02:37 +0900

    [add]while2

---
+++ While2.java
@@ -0,0 +1,15 @@
+class While2 {
+  public static void main(String[] args) {
+    int a[] = {10, 3, -1, 5, 20};
+    int i = 0;
+    do {
+        if (a[i] < 0) {
+            System.out.print("i=" + i + " でループを抜ける");
+            System.out.println(" a[" + i + "]: " + a[i]);
+            break;
+        }
+        System.out.println("i=" + i + " の処理");
+        ++i;
+    } while (i < 5);
+  }
+}
