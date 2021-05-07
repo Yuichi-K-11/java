4db0571081e78bf7c7a26c91db46b222e034495f
Yuichi-K-11 <banaichi1@gmail.com>
2021-05-07 23:22:44 +0900

    [add]while3

---
+++ While3.java
@@ -0,0 +1,11 @@
+class While2 {
+  public static void main(String[] args) {
+    int a[] = new int[10];
+    int i = 0;
+    while (i < 10) {
+        a[i] = i * 10;
+        ++i;
+    }
+    System.out.println("a[9]: " + a[9]);
+  }
+}
