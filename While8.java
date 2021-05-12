f3cdcc20f93e8f2b8322dbb4c69a7246dd86e686
Yuichi-K-11 <banaichi1@gmail.com>
2021-05-12 23:49:59 +0900

    [add]while8

---
+++ While8.java
@@ -0,0 +1,18 @@
+class While8 {
+  public static void main(String[] args) {
+
+
+
+        try {
+            FileReader fr = new FileReader(new File("test.txt"));
+
+            int ch = fr.read(); // 1文字読み込み
+            while (ch != -1) { //読み込んだ文字があれば
+                System.out.println((char)ch);
+                ch = fr.read(); // 次の文字を読み込み
+  }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
