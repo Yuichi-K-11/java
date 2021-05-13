deb54dd084a4e6ecb610194de6b51af2ede2b51d
Yuichi-K-11 <banaichi1@gmail.com>
2021-05-13 23:57:00 +0900

    [add]score

--- While8.java
+++ While8.java
@@ -1,18 +1,14 @@
 class While8 {
   public static void main(String[] args) {
+    try {
+    FileReader fr = new FileReader(new File("test.txt"));
 
-
-
-        try {
-            FileReader fr = new FileReader(new File("test.txt"));
-
-            int ch = fr.read(); // 1文字読み込み
-            while (ch != -1) { //読み込んだ文字があれば
-                System.out.println((char)ch);
-                ch = fr.read(); // 次の文字を読み込み
+    int ch = fr.read(); // 1文字読み込み
+    while (ch != -1) { //読み込んだ文字があれば
+        System.out.println((char)ch);
+        ch = fr.read(); // 次の文字を読み込み
   }
         } catch (Exception e) {
             System.out.println(e);
         }
-    }
 }
