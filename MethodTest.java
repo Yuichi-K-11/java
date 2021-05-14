46af9a19bee033d020a643caec2dfc7010d58788
Yuichi-K-11 <banaichi1@gmail.com>
2021-05-14 23:47:25 +0900

    [add]methodtest

---
+++ MethodTest.java
@@ -0,0 +1,10 @@
+//coding:utf-8
+class MethodTest{
+   public static void main(String[] args){
+      System.out.println("mainメソッドが呼ばれました");
+      sub();
+   }
+   public static void sub(){
+      System.out.println("subメソッドが呼ばれました");
+   }
+}
