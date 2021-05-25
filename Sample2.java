0c5d73ad41891424910e407b79e4faba2a95f5d2
euronflorida11 <banaichi1@gmail.com>
2021-05-25 23:42:32 +0900

    [add]sample3

--- Sample2.java
+++ Sample2.java
@@ -1,15 +1,18 @@
-public class Sample2 {
-    //  フィールド
-    int n = 10;
-    String s = "field";
-    //  メソッド
-    int add(int a,int b){
-        return a + b;
+class Sample2 {
+  public static void main(String[] args)
+
+      //  フィールド
+      int n = 10;
+      String s = "field";
+      //  メソッド
+      int add(int a,int b){
+          return a + b;
+      }
+      String add(String s){
+          return this.s + s;
+      }
+      void showNum(){
+          System.out.println("n = " + n);
     }
-    String add(String s){
-        return this.s + s;
-    }
-    void showNum(){
-        System.out.println("n = " + n);
-    }
+  }
 }
