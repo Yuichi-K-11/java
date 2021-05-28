bee886f1365a341dc8cc5b3fb99b316123370bc7
euronflorida11 <banaichi1@gmail.com>
2021-05-28 19:18:24 +0900

    [add]buffer2

---
+++ Buffer2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Buffer2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 入力パラメータを読み込む
+        String input = br.readLine();
+        String[] param = input.split(" ");//入力値を空白で分解する
+        int num1 = Integer.parseInt(param[0]);
+        String type = param[1] ;
+        int num2 = Integer.parseInt(param[2]);
+        if(type.equals("+")){ //①
+            System.out.println(num1 + num2);
+        }else if(type.equals("-")){
+            System.out.println(num1 - num2);
+        }else if(type.equals("*")){
+            System.out.println(num1 * num2);
+        }else if(type.equals("/")){
+            System.out.println(num1 / num2 + " ... " +  num1 % num2);//②
+        }
+    }
+}
